244442e20923b03f003d963673964173d7d7a9c2
cbermudez <cesar.bermudez@uruit.com>
2021-06-16 09:51:49 -0500

    10- Query String Parameters

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import querString from "query-string";
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
+  const result = querString.parse(location.search);
+  console.log(result);
   return (
     <div>
       <h1>Posts</h1>
