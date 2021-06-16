aa2fecd0acc6f4e37aacf8c77135a4e381dff02f
cbermudez <cesar.bermudez@uruit.com>
2021-06-16 08:49:58 -0500

    10- Query String Parameters

--- src/App.js
+++ src/App.js
@@ -12,21 +12,27 @@ import NotFound from "./components/notFound";
 import "./App.css";
 
 class App extends Component {
-	render() {
-		return (
-			<div>
-				<NavBar />
-				<div className="content">
-					<Switch>
-						<Route path="/products" component={Products}></Route>
-						<Route path="/posts" component={Posts}></Route>
-						<Route path="/admin" component={Dashboard}></Route>
-						<Route path="/" component={Home}></Route>
-					</Switch>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/products/:id" component={ProductDetails}></Route>
+            <Route
+              path="/products"
+              render={(props) => {
+                return <Products sortBy="neweast" {...props} />;
+              }}
+            ></Route>
+            <Route path="/posts/:year?/:month?" component={Posts}></Route>
+            <Route path="/admin" component={Dashboard}></Route>
+            <Route path="/" component={Home}></Route>
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
